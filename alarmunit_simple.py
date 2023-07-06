0898e8e6ab1d4676f03b3e965908802a1f4988cd
Philip Groß <philip.gross@geow.uni-heidelberg.de>
2023-07-06 12:02:13 +0200

    add centralized error tracking

--- alarmunit_simple.py
+++ alarmunit_simple.py
@@ -9,19 +9,20 @@ Created on Tue Jul  4 16:21:15 2023
 from machine import Pin, ADC, Timer
 from Error import Error
 
-# Digital input pins
+# Digital pins
 digital_input_pin = Pin(3, Pin.IN)
-digital_output_pin = Pin(4, Pin.OUT)
+digital_error_pin = Pin(4, Pin.OUT)
 digital_error_value = 1
+analog_error_pin = Pin(5, Pin.OUT)
+no_errors_pin = Pin(6, Pin.OUT)
 
 # Analog input pins
 analog_input_pin = ADC(Pin(26))
-analog_output_pin = Pin(5, Pin.OUT)
 analog_error_threshold = 5000
 
 # Create instances Error class
-digital_error = Error(digital_input_pin, digital_output_pin, digital_error_value)
-analog_error = Error(analog_input_pin, analog_output_pin, analog_error_threshold)
+digital_error = Error(digital_input_pin, digital_error_pin, digital_error_value)
+analog_error = Error(analog_input_pin, analog_error_pin, analog_error_threshold)
 
 # Timer callback function
 def timer_callback(timer):
@@ -30,6 +31,9 @@ def timer_callback(timer):
 
     # Check for analog error
     analog_error.check_analog()
+    
+    # Check for no errors and light pin
+    Error.handle_no_errors(no_errors_pin)
     
     # Sleep is NOT needed in this implementation!
 
