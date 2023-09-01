cdc57a60ababe441476c96d84061f894a975db41
pschw26 <peter.schwoerer@stud.uni-heidelberg.de>
2023-09-01 16:40:42 +0200

    overdone some documentation on mcp_pin_class

--- mcp_pin_class.py
+++ mcp_pin_class.py
@@ -6,11 +6,12 @@ class mcp_pin:
        taken arguments:
            i2c: takes chosen i2c-bus
            device_addr: hardware configuration for i2c device address,
-           bank: port-A or B of MCP23017
+           bank: bank-A or B of MCP23017
            pin: decimal number for GPIO-A or B register (takes values from 0 to 7)
-           (Note: port-A counts pins from right to left, port-B from left to right!
+           (Note: bank-A counts pins from right to left, bank-B from left to right!
            ->see MCP-datasheet)
-           io: takes 0 for output and 1 for input'''
+           io: takes 0 for output and 1 for input on corresponding bank
+           (Note: the given number sets ALL pins on one bank to in- or output)'''
     def __init__(self, i2c, device_addr, bank, pin, io):
         self.i2c = i2c
         self.device_addr = device_addr
@@ -26,8 +27,8 @@ class mcp_pin:
             self.regist_addr = 0x12
         elif self.bank == 0x01:
             self.regist_addr = 0x13
-        '''init for I/O configuration on port/bank-A or B of device.
-        self.io sets specified port-bank to in- or output:
+        '''init for I/O configuration on bank-A or B of device.
+        instance.io sets specified bank to in- or output:
         0 means output / 1 means input.'''
         if self.io == 0:
             self.io == 0x00
@@ -39,21 +40,21 @@ class mcp_pin:
 
     def on(self):
         '''everytime on() or off() get called, byte which is sent to self.regist_addr
-        of device with self.device_addr refreshes pin state of all 8 pins on bank/port.
+        of device with self.device_addr refreshes pin state of all 8 pins on chosen port.
         state-byte is realized by classvariable state, which gets overwritten according to instance.pin'''
         mask = 1 << self.pin
         mcp_pin.state[0] |= mask
-        self.i2c.writeto_mem(self.device_addr, self.regist_addr, mcp_pin.state)#TODO
+        self.i2c.writeto_mem(self.device_addr, self.regist_addr, mcp_pin.state)
         
 
     def off(self):
-        '''off() just inverts ones for specified pin others remain untouched.'''
+        '''off() just inverts corresponding bit for instance.pin, others remain untouched.'''
         mask = 1 << self.pin
         mcp_pin.state[0] &= ~mask
-        self.i2c.writeto_mem(self.device_addr, self.regist_addr, mcp_pin.state)#TODO
+        self.i2c.writeto_mem(self.device_addr, self.regist_addr, mcp_pin.state)
         
     def value(self):
-        '''takes value of bit according to self.pin from GPIO-A or B register byte'''
+        '''takes value of bit according to instance.pin from GPIO-A or B register byte'''
         value = self.i2c.readfrom_mem(self.device_addr, self.regist_addr, 1)
         mask = 1 << self.pin
         if value[0] & mask:
