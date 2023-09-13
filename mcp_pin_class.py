24ecc5441d9da467079a6ab636e4d05b074a5bc7
pschw26 <peter.schwoerer@stud.uni-heidelberg.de>
2023-09-13 15:20:20 +0200

    more documentation/ made attempt to fix norm_val problem in value()

--- mcp_pin_class.py
+++ mcp_pin_class.py
@@ -1,30 +1,32 @@
 
 class mcp_pin:
-    state = bytearray([0])
+    state = bytearray([0]) # classvariable to memorize all states of output pins in current iteration
     '''class for adapting i2c syntax of MCP23017 device to micropython,
        to provide compatibility with machine functions. 
        taken arguments:
-           i2c: takes chosen i2c-bus
-           device_addr: hardware configuration for i2c device address
+           i2c: type = hex; takes chosen i2c-bus
+           device_addr: type = hex; hardware configuration for i2c device address
            string[0]:
-               bank: bank-A or B of MCP23017
+               bank: type = str; 'a' or 'b' for bank-A or B of MCP23017
            string[1]:
-               pin: decimal number for GPIO-A or B register (takes values from 0 to 7)
+               pin: type = str; decimal number for GPIO-A or B register (takes values from 0 to 7)
                (Note: bank-A counts pins from right to left, bank-B from left to right!
                ->see MCP-datasheet)
-           io: takes OUT for output and IN for input on corresponding bank
-           (Note: the given string sets ALL pins on one bank to in- or output)'''
-    def __init__(self, i2c, device_addr, string, io):
+           io: type = str; takes 'OUT' for output and 'IN' for input on corresponding bank
+           (Note: the given string sets ALL pins on one bank to in- or output)
+           nonc: type = int; optional argument for input pins. 1 for normally opened, 0 for normally closed'''
+    def __init__(self, i2c, device_addr, string, io, nonc = None):
         self.i2c = i2c
         self.device_addr = device_addr
         self.bank = string[0]
         self.pin = int(string[1])
         self.io = io
+        self.nonc = nonc
         self.setup()
         
     def setup(self):
         '''defining GPIOA or GPIOB as output address for functions
-        from IODIRA and IODIRB register'''
+        from IODIRA and IODIRB register.'''
         if self.bank == 'a':
             self.bank = 0x00
             self.regist_addr = 0x12
@@ -42,36 +44,42 @@ class mcp_pin:
         self.i2c.writeto_mem(self.device_addr, self.bank, bytearray([self.io]))
 
     def on(self):
-        '''everytime on() or off() get called, byte which is sent to self.regist_addr
-        of device with self.device_addr refreshes pin state of all 8 pins on chosen port.
-        state-byte is realized by classvariable state, which gets overwritten according to instance.pin'''
+        '''everytime on() or off() get called, byte which is sent to instance.regist_addr
+        of device with instance.device_addr refreshes pin state of all 8 pins on chosen port.
+        state-byte is realized by class.state, which gets manipulated according to instance.pin.'''
         try:
-            mask = 1 << self.pin
-            mcp_pin.state[0] |= mask
+            mask = 1 << self.pin # mask shifts value 1 in 0x01 byte to digit given by instance.pin 
+            mcp_pin.state[0] |= mask # mask gets applied to the first byte in the bytearray class.state,
+                                     # bitwise-OR-operator; only converts 0 to 1 at desired digit
             #print(mcp_pin.state)
-            self.i2c.writeto_mem(self.device_addr, self.regist_addr, mcp_pin.state)
+            self.i2c.writeto_mem(self.device_addr, self.regist_addr, mcp_pin.state) # new state gets transfered to given register
         except OSError as e:
-            print(f"Error: I/O-access in on(): {e}")
+            print(f"Error: I/O-access in on(): {e}") # in case of trouble of communication between mcp and pico
 
     def off(self):
         '''off() just inverts corresponding bit for instance.pin, others remain untouched.'''
         try:
-            mask = 1 << self.pin
-            mcp_pin.state[0] &= ~mask
+            mask = 1 << self.pin # mask shifts value 1 in 0x01 byte to digit given by instance.pin
+            mcp_pin.state[0] &= ~mask # inverted mask gets applied, bitwise-AND-operator; only converts 1 to 0 at desired digit
             #print(mcp_pin.state)
-            self.i2c.writeto_mem(self.device_addr, self.regist_addr, mcp_pin.state)
+            self.i2c.writeto_mem(self.device_addr, self.regist_addr, mcp_pin.state) # new state gets transfered to given register
         except OSError as e:
-            print(f"Error: I/O-access in in off(): {e}")
+            print(f"Error: I/O-access in in off(): {e}") # in case of trouble of communication between mcp and pico
         
     def value(self):
-        '''takes value of bit according to instance.pin from GPIO-A or B register byte'''
+        '''compares mask-byte via bitwise-AND with byte at register-address of bank-A or B depending on which is set to 'IN'.'''
         try:
-            value = self.i2c.readfrom_mem(self.device_addr, self.regist_addr, 1)
+            value = self.i2c.readfrom_mem(self.device_addr, self.regist_addr, 1) # acquire first byte from register
             mask = 1 << self.pin
-            if value[0] & mask:
-                return 1
+            #print(value)
+            if value[0] & mask: # returns no error if value[0] and instance.pin share set bit on same position
+                # no error
+                return 1 # TODO: return self.norm_val should get taken, works otherwise only for norm_val = 1
+                # return self.nonc
             else:
-                return 0
+                # error
+                return 0 # abs(self.norm_val-1)
+                # return abs(self.nonc-1)
         except OSError as e:
-            print(f"Error: I/O-access in in value(): {e}")
+            print(f"Error: I/O-access in in value(): {e}") # in case of trouble of communication between mcp and pico
     
